f2ce9b52ca35a56c5db403cbbb5a2693545bb425
naiveashish6@gmail.com <sourav.singh001309@gmail.com>
2023-04-04 12:46:50 +0530

    arrays sum

--- src/naive/arrays/Arrays.java
+++ src/naive/arrays/Arrays.java
@@ -1,5 +1,30 @@
 package naive.arrays;
 
+import java.util.Scanner;
+
 public class Arrays {
-    // sum 
+    // sum of elements in arrays
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the test cases");
+        int t = sc.nextInt();
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        int a [] = new int[n];
+        while(t!=0){
+            for(int i = 0;  i< n ; i++ ) {
+                System.out.println("enter element at " + i);
+                a[i] = sc.nextInt();
+            }
+            int sum = 0;
+            for(int i = 0;  i< n ; i++ ) {
+                sum =  sum + a[i];
+            }
+            System.out.println("The sum is : "+sum);
+            t--;
+
+        }
+    }
+
+
 }
